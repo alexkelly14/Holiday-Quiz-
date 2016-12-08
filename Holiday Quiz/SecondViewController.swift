6393dc5a0cbe5986324e9a3549f2de898fb55525
akelly <alexandrakelly@alexandras-macbook-air-5.local>
2016-12-08 11:20:14 -0600

    Initial Commit

---
+++ Holiday Quiz/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  Holiday Quiz
+//
+//  Created by Alexandra Kelly  on 12/7/16.
+//  Copyright © 2016 Alexandra Kelly . All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Holiday Quiz/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Holiday Quiz
+//
+//  Created by Alexandra Kelly  on 12/6/16.
+//  Copyright © 2016 Alexandra Kelly . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func prepare(for segue : UIStoryboardSegue, sender: Any?) {
+        let dvc = segue.destination as! SecondViewController
+}
+
+}
