2230a66e96d00604ce6fcfb6e8d58048cfbaaf65
akelly <alexandrakelly@alexandras-macbook-air-5.local>
2016-12-13 11:55:20 -0600

    I make a questions array and a loadQuestion function the load question function takes out a question from the array and doesn't allow it to use it again. Then  I made button tag = 0 and when a button is clicked all of the others can not be clicked.

---
+++ Holiday Quiz/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Holiday Quiz
+//
+//  Created by Alexandra Kelly  on 12/13/16.
+//  Copyright © 2016 Alexandra Kelly . All rights reserved.
+//
+
+import UIKit
+
+class Question: NSObject {
+    var question = ""
+    var answer0 = ""
+    var answer1 = ""
+    var answer2 = ""
+    var answer3 = ""
+    var correct = 0
+
+}
--- Holiday Quiz/SecondViewController.swift
+++ Holiday Quiz/SecondViewController.swift
@@ -9,140 +9,160 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answer1Button: UIButton!
     @IBOutlet weak var answer0Button: UIButton!
     @IBOutlet weak var answer2Button: UIButton!
     @IBOutlet weak var answer3Button: UIButton!
-    var correctAnswer = String()
+    var questions = [Question]()
+    var correctAnswer = 0
+    var answer = 0
+    var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     chooseQuestion()
+        resetColors()
+        loadQuestions()
+        chooseQuestion()
     }
+    
+    func displayAnswer() {
+        if(answer0Button.tag == 0){
+            buttonsOff()
+            if correctAnswer == 0
+            {
+                answer0Button.backgroundColor = UIColor.green
+            }
+            else if correctAnswer == 1
+            {
+                answer1Button.backgroundColor = UIColor.green
+            }
+            else if correctAnswer == 2
+            {
+                answer2Button.backgroundColor = UIColor.green
+            }
+            else if correctAnswer == 3
+            {
+                answer3Button.backgroundColor = UIColor.green
+            }
+            if correctAnswer == answer {
+                score += 1
+            }
+        }
+    }
+    
     @IBAction func onAnswer0Tapped(_ sender: UIButton) {
-        if correctAnswer == "0"
-        {
-            ifAnswerCorrect()
-        answer0Button.backgroundColor = UIColor.green
-        }
-        else
-        {
-            ifAnswerWrong()
-        answer0Button.backgroundColor = UIColor.red
-        }
+        answer = 0
+        displayAnswer()
     }
     
     @IBAction func onAnwer1Tapped(_ sender: UIButton) {
-        if correctAnswer == "1"
-        {
-            ifAnswerCorrect()
-            answer1Button.backgroundColor = UIColor.green
-        }
-        else
-        {
-            ifAnswerWrong()
-            answer1Button.backgroundColor = UIColor.red
-        }
+        answer = 1
+        displayAnswer()
     }
     
     @IBAction func onAnswer2Tapped(_ sender: UIButton) {
-        if correctAnswer == "2"
-        {
-            ifAnswerCorrect()
-            answer2Button.backgroundColor = UIColor.green
-        }
-        else
-        {
-            ifAnswerWrong()
-            answer2Button.backgroundColor = UIColor.red
-        }
+        answer = 2
+        displayAnswer()
     }
     
     @IBAction func onAnswer3Tapped(_ sender: UIButton) {
-        if correctAnswer == "3"
-        {
-            ifAnswerCorrect()
-            answer3Button.backgroundColor = UIColor.green
-        }
-        else
-        {
-            ifAnswerWrong()
-            answer3Button.backgroundColor = UIColor.red
-        }
+        answer = 3
+        displayAnswer()
     }
     
     @IBAction func onNextTapped(_ sender: UIButton) {
         chooseQuestion()
-        restColors()
+        resetColors()
     }
+    
+    func buttonsOff() {
+        answer0Button.tag = 1
+        answer1Button.tag = 1
+        answer2Button.tag = 1
+        answer3Button.tag = 1
+        answer0Button.backgroundColor = UIColor.red
+        answer1Button.backgroundColor = UIColor.red
+        answer2Button.backgroundColor = UIColor.red
+        answer3Button.backgroundColor = UIColor.red
+    }
+    
+    func loadQuestions() {
+        var question = Question()
+        
+        // question 0
+        question.question = "What is Scrooge's first name?"
+        question.answer0 = "Jim"
+        question.answer1 = "Ebinezer"
+        question.answer2 = "Nicholas"
+        question.answer3 = "Tiny Tim"
+        question.correct = 1
+        questions.append(question)
+        
+        // question 1
+        question = Question()
+        question.question = "What is the Grinch's Dog named?"
+        question.answer0 = "Max"
+        question.answer1 = "Mark"
+        question.answer2 = "Mat"
+        question.answer3 = "Sam"
+        question.correct = 0
+        questions.append(question)
+        
+        //question 2 
+        question = Question()
+        question.question = "Buddy the Elf traveled through everything EXECPT"
+        question.answer0 = "Lincoln Tunnel"
+        question.answer1 = "Candy Cane Forest"
+        question.answer2 = "Swirly Twirly Gumdrops"
+        question.answer3 = "Marshallow Meadow"
+        question.correct = 3
+        questions.append(question)
+        
+        //question 3
+        question = Question()
+        question.question = "What American President banned Christmas Trees from the White House?"
+        question.answer0 = "FDR"
+        question.answer1 = "Lincoln"
+        question.answer2 = "Washington"
+        question.answer3 = "Theodore Roosevelt"
+        question.correct = 3
+        questions.append(question)
+        
+        //question 4
+        question = Question()
+        question.question = "In the Polar Express movie, what is the First Gift of Christmas?"
+        question.answer0 = "Santa Hat"
+        question.answer1 = "Doll House"
+        question.answer2 = "Bell"
+        question.answer3 = "Puppy"
+        question.correct = 2
+        questions.append(question)
+   }
+    
     func chooseQuestion() {
-        var questionNumber = arc4random() % 5
-        switch (questionNumber) {
-        case 0:
-            questionLabel.text = "What is Scrooge's first name?"
-            answer0Button.setTitle("Jim", for: UIControlState.normal)
-            answer1Button.setTitle("Ebenezer", for: UIControlState.normal)
-            answer2Button.setTitle("Nicholas", for: UIControlState.normal)
-            answer3Button.setTitle("Tiny Tim", for: UIControlState.normal)
-            correctAnswer = "1"
-            break
-        case 1:
-            questionLabel.text = "What is the Grinch's Dog named?"
-            answer0Button.setTitle("Max", for: UIControlState.normal)
-            answer1Button.setTitle("Mat", for: UIControlState.normal)
-            answer2Button.setTitle("Mark", for: UIControlState.normal)
-            answer3Button.setTitle("Sam", for: UIControlState.normal)
-            correctAnswer = "0"
-            break
-        case 2:
-            questionLabel.text = "Buddy the Elf traveled through everything EXECPT"
-            answer0Button.setTitle("Lincoln Tunnel", for: UIControlState.normal)
-            answer1Button.setTitle("Candy Cane Forest", for: UIControlState.normal)
-            answer2Button.setTitle("Swirly Twirly Gumdrops", for: UIControlState.normal)
-            answer3Button.setTitle("Marshmallow Meadow", for: UIControlState.normal)
-            correctAnswer = "3"
-            break
-        case 3:
-            questionLabel.text = "What American President banned Christmas Trees from the White House?"
-            answer0Button.setTitle("Lincoln", for: UIControlState.normal)
-            answer1Button.setTitle("Theodore Roosevelt", for: UIControlState.normal)
-            answer2Button.setTitle("FDR", for: UIControlState.normal)
-            answer3Button.setTitle("Washington", for: UIControlState.normal)
-            correctAnswer = "2"
-            break
-        case 4:
-            questionLabel.text = "In the Polar Express movie, what is the First Gift of Christmas?"
-            answer0Button.setTitle("Santa Hat", for: UIControlState.normal)
-            answer1Button.setTitle("Puppy", for: UIControlState.normal)
-            answer2Button.setTitle("Doll House", for: UIControlState.normal)
-            answer3Button.setTitle("A Bell", for: UIControlState.normal)
-            correctAnswer = "3"
-            break
-        default:
-            break
-        }
+        let index = Int(arc4random_uniform(UInt32(questions.count)))
+        let question = questions[index]
+        questions.remove(at: index)
+        questionLabel.text = question.question
+        answer0Button.setTitle(question.answer0, for: UIControlState.normal)
+        answer1Button.setTitle(question.answer1, for: UIControlState.normal)
+        answer2Button.setTitle(question.answer2, for: UIControlState.normal)
+        answer3Button.setTitle(question.answer3, for: UIControlState.normal)
+        correctAnswer = question.correct
     }
-    func restColors(){
+    
+    func resetColors(){
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "xmas tree2")!)
         answer0Button.backgroundColor = UIColor.clear
         answer1Button.backgroundColor = UIColor.clear
         answer2Button.backgroundColor = UIColor.clear
         answer3Button.backgroundColor = UIColor.clear
-    }
-    func ifAnswerCorrect() {
-        answer0Button.backgroundColor = UIColor.green
-        answer1Button.backgroundColor = UIColor.green
-        answer2Button.backgroundColor = UIColor.green
-        answer3Button.backgroundColor = UIColor.green
-    }
-    func ifAnswerWrong() {
-        answer0Button.backgroundColor = UIColor.red
-        answer1Button.backgroundColor = UIColor.red
-        answer2Button.backgroundColor = UIColor.red
-        answer3Button.backgroundColor = UIColor.red
-
+        answer0Button.tag = 0
+        answer1Button.tag = 0
+        answer2Button.tag = 0
+        answer3Button.tag = 0
     }
 }
 
